9ca7274495ae3837eebd0415bce91b39883c50da
JoaoArthurSouzaSantos <joaoarthursouzasantos@gmail.com>
2024-12-17 09:35:41 -0300

    cria modelo,factory,seeder e migration de produto

---
+++ Merceariasysten/database/factories/ProdutoFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Produto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProdutoFactory extends Factory
+{
+    protected $model = Produto::class;
+
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->word(),
+            'descricao' => $this->faker->sentence(),
+            'preco' => $this->faker->randomFloat(2, 1, 1000),
+            'quantidade_estoque' => $this->faker->numberBetween(1, 100),
+        ];
+    }
+}
